c8d6e4e3f23c460872f9e528cba846fe8598a84c
Damir Lapeiko <lapeiko02@maik.ru>
2023-02-27 11:52:09 +0300

    Creating realization for feature belarusian passport

---
+++ src/by/teachmeskills/lapeiko/homework15/Homework15.java
@@ -0,0 +1,56 @@
+package by.teachmeskills.lapeiko.homework15;
+
+import by.teachmeskills.lapeiko.homework15.task1.BlrPassport;
+import by.teachmeskills.lapeiko.homework15.task1.Gender;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Homework15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                """
+                        Choose task:
+                        1. Belarusian passport
+                        2. Black words filter
+                        0. Exit"""
+        );
+        while (true) {
+            System.out.print("Task number: ");
+            int taskNumber = Integer.parseInt(scanner.nextLine());
+            switch (taskNumber) {
+                case 1 -> {
+                    System.out.print("Write your first name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("Write your last name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Write your gender (MALE of FEMALE): ");
+                    Gender gender = Gender.valueOf(scanner.nextLine());
+                    System.out.print("Write your date of birth (dd.MM.yyyy): ");
+                    LocalDate dateOfBirth = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                    System.out.print("Write your passport number: ");
+                    String passportNumber = scanner.nextLine();
+                    System.out.print("Write your ID number of passport: ");
+                    String idNumber = scanner.nextLine();
+                    System.out.print("Write your date of issue of passport (dd.MM.yyyy): ");
+                    LocalDate dateOfIssue = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                    System.out.print("Write your validity of passport (dd.MM.yyyy): ");
+                    LocalDate validityOfPassport = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                    BlrPassport blrPassport = new BlrPassport(firstName, lastName, gender, dateOfBirth, passportNumber,
+                            idNumber, dateOfIssue, validityOfPassport);
+                    System.out.println(blrPassport);
+                    System.out.println("Is your passport expired: " + blrPassport.isExpired(LocalDate.now(ZoneId.of("Europe/Minsk"))));
+                }
+//                case 2 -> {
+//                }
+//                case 0 -> {
+//                    return;
+//                }
+//                default -> System.err.println("Task doesn't exist");
+            }
+            System.out.println();
+        }
+    }
+}
